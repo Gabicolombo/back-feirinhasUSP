341cef530e1c00a7eb8604b1c2259fe1668257ff
Gabriela Colombo <65355183+Gabicolombo@users.noreply.github.com>
2022-11-15 14:49:00 -0300

    hospedando

--- src/database/mongoConnection.js
+++ src/database/mongoConnection.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 module.exports = async() => {
     try{
-        const url = "mongodb://localhost/FeirinhasUsp";
+        // const url = "mongodb://localhost/FeirinhasUsp";
+        const url = "mongodb+srv://feirinha:feirinha-usp@feirinha-usp-rp2.uherujz.mongodb.net/test"
         await mongoose.connect(url);
         console.log('Connected to database');
     }catch(err){
